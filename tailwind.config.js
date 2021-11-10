b066cdb8d27b1b89f6b4a1ca24c69cc875f95cac
devlassev <vestergaardlasse2@gmail.com>
2021-11-10 19:45:27 +0100

    update files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["*.html"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Fira sans", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
